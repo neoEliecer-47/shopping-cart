87c7b14c29db4d527d24ffefd5111acb8a0c13a5
eliecer <eliecer.sanchez47@gmail.com>
2023-09-26 22:35:21 +0200

    slice at CartContext

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -13,32 +13,38 @@ const updateLocalStorage = state => {
 
 const reducer = (state, action) => {//basicamente transforma el estado dependiendo de la accion, y retorna ese nuevo estado calculado: sea nuevo o viejo
   const { type, payload } = action//el type es la accion y el payload es el objeto para actualizar el estado (el producto en este caso)
+  console.log(state)
+  console.log(payload)
 
   switch (type){
     case 'ADD_TO_CART' : {
-      const productInCartIndex = state.findIndex(item => item.id === payload.id)
+      const productInCartIndex = state.findIndex(item => item.id === payload.id)//retorna un -1 si no lo encuentra
       if(productInCartIndex >= 0){
-        const newState = structuredClone(state);//hace copias profundas, por lo tanto, aqui tenemos un carrito nuevo
+       /* const newState = structuredClone(state);//hace copias profundas, por lo tanto, aqui tenemos un carrito nuevo
         newState[productInCartIndex].quantity += 1
         updateLocalStorage(newState)
-        return newState
-  
-      }
-
-      const newState = [
-        ...state, {...payload, quantity: 1}//en caso de que el producto que estamos intentando pasar no existe en el array(payload = product)
-      ]
-      const newState2 = [...state.slice(0, productInCartIndex), //agrega los productos desde el index 0 hasta el productCartIndex -1
+        return newState*/
+        const newState2 = [...state.slice(0, productInCartIndex), //agrega los productos desde el index 0 hasta el productCartIndex -1
         {...state[productInCartIndex], quantity: state[productInCartIndex].quantity + 1 },//exactamente en el index encontgrado agrega el producto y suman la cantidad de uno. +1
         ...state.slice(productInCartIndex + 1)//agrega el resto de productos que van despues del indice encontrado. ya que el slice solo trabaja con un indice start y un end, siendo el end -1 en la posicion del array
       ]
+  
+      }
+      const newState = [
+        ...state, {...payload, quantity: 1}//en caso de que el producto que estamos intentando pasar no existe en el array(payload = product)
+      ]
+    /*  const newState2 = [...state.slice(0, productInCartIndex), //agrega los productos desde el index 0 hasta el productCartIndex -1
+        {...state[productInCartIndex], quantity: state[productInCartIndex].quantity + 1 },//exactamente en el index encontgrado agrega el producto y suman la cantidad de uno. +1
+        ...state.slice(productInCartIndex + 1)//agrega el resto de productos que van despues del indice encontrado. ya que el slice solo trabaja con un indice start y un end, siendo el end -1 en la posicion del array
+      ]*/
+      //console.log({new2: newState2})
 
       updateLocalStorage(newState)
       return newState
     }
-
+    
     case 'REMOVE_FROM_CART' : {
-      const newState = state.filter(item => item.id !== product.id)//prev es el estado anterior
+      const newState = state.filter(item => item.id !== payload.id)//prev es el estado anterior
       updateLocalStorage(newState)
       return newState
     }
@@ -58,7 +64,7 @@ const CartProvider = ({ children }) => {
  })
  const removeFromCart = (product) => dispatch({
   type: 'REMOVE_FROM_CART',
-  
+  payload: product
  })
 
  const clearCart = (product) => dispatch({
